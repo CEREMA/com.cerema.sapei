96f7970e7f3ef7305eba29d1fb5b8be429c6f16f
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-19 12:58:00 +0100

    Update# 209

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -629,7 +629,7 @@ App.controller.define('CMain', {
     {
 		App.reset(App.get('TAffaire')); 
         var record=App.get('VOpenAffaire grid').getSelectionModel().getSelection();
-        
+        console.log(record);
 		App.DB.get('sapei://job{*,axe.Axe,axe.dpt.IdDepartement}?Id_job='+record.data.Id_job,App.get('TAffaire'),function(response) {
             
 			response=response.data[0];
