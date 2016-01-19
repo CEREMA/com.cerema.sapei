5860bbe1c444de7bca0d597a91d713c8fea58aef
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-19 13:27:53 +0100

    Update# 222

--- src/Contents/Application/app/view/VCarnet.js
+++ src/Contents/Application/app/view/VCarnet.js
@@ -2,7 +2,7 @@ App.view.define('VCarnet', {
     extend: "Ext.window.Window",
     alias: 'widget.VCarnet',
     initComponent: function() {
-        this.width = 800;
+        this.width = 600;
         this.height = 660;
 
         this.layout = {
@@ -30,6 +30,38 @@ App.view.define('VCarnet', {
                 store: App.store.create('sapei://client_origine',{autoLoad: true}),
                 displayField: "Lib_client_origine",
                 valueField: "Id_client_origine"
+            },
+            {
+                flex: 1,
+                border: false
+            },
+            {
+                xtype: "grid",
+                width: "100%",
+                columns:[
+                {
+                    dataIndex: "NomPrenom_contact_client"
+                },
+                {
+                    dataIndex: "Tel_contact_client" 
+                },
+                {
+                    dataIndex: "Mobile_contact_client" 
+                },
+                {
+                    dataIndex: "Mail_contact_client" 
+                },
+                {
+                    dataIndex: "Fonction_contact_client" 
+                },
+                {
+                    dataIndex: "Lib_client_rattache"
+                },
+                {
+                    dataIndex: "Lib_type_contact"
+                }
+                ],
+                store: App.store.create('contact_client{*,client_rattache.Lib_client_rattache,type_contact.Lib_type_contact}',{autoLoad: true})
             }
 		];
 
