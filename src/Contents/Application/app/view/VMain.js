91ee687dd20d056230ad0fb64950e93654669586
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-19 10:44:44 +0100

    Update# 182

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -50,6 +50,17 @@ App.view.define('VMain', {
             {
 				xtype: "VSchedulerMain",
 				hidden: false,
+                bbar: [
+                    '->',
+                    {
+                        xtype: "panel",
+                        border: false,
+                        height: 35,
+                        html: '<table><tr><td><div style="background-color:yellow;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Option</td><td><div style="background-color:orange;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Pré-réservation</td><td><div style="background-color:green;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Réservation</td><td><div style="background-color:red;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Maintenance</td></tr></table>',
+                        bodyStyle: "background-color:transparent",
+                        itemId: "Legendes"
+                    }
+                ],                            
 				flex: 1                
             }
 			]
