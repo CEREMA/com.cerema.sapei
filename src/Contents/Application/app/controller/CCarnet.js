ab5e6fb6f063569c2424c64bf548099fe4e5c41a
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-19 13:56:15 +0100

    Update# 235

--- src/Contents/Application/app/controller/CCarnet.js
+++ src/Contents/Application/app/controller/CCarnet.js
@@ -11,9 +11,16 @@ App.controller.define('CMain', {
 	{
 
 		this.control({
-            "VCarnet combo#    
+            "VCarnet combo#client": {
+                select: "client_select"    
+            }
 		});
 				
 	},
+    client_select: function(p)
+    {
+        App.get('VCarnet combo#service').getStore().getProxy().extraParams.Id_client_origine=p.getValue();
+        App.get('VCarnet combo#service').getStore().load();
+    }
 	
 });
