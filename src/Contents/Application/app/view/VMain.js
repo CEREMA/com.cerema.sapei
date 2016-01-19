7b1b8807df71ca330fc84d13b0c897b74d71dd7b
Stéphane Zucatti <stephane.zucatti@me.com>
2016-01-19 10:44:30 +0100

    Update# 181

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -41,17 +41,6 @@ App.view.define('VMain', {
 			split:true,
 			layout: "fit",
             border: false,
-			bbar: [
-				'->',
-				{
-					xtype: "panel",
-					border: false,
-					height: 35,
-					html: '<table><tr><td><div style="background-color:yellow;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Option</td><td><div style="background-color:orange;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Pré-réservation</td><td><div style="background-color:green;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Réservation</td><td><div style="background-color:red;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Maintenance</td></tr></table>',
-					bodyStyle: "background-color:transparent",
-					itemId: "Legendes"
-				}
-			],            
 			items: [
 			{
 				xtype: "TAffaire",
