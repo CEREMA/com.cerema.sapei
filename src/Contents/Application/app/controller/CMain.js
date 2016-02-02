d5373de104eec913c622ee76ea411e08b8849727
Stéphane Zucatti <stephane.zucati@me.com>
2016-02-02 16:47:12 +0100

    Update# 372

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -857,7 +857,7 @@ App.controller.define('CMain', {
 						var tpl=[];
 						for (var i=0;i<r.result.data.length;i++) {
 							var results=html;
-							results=results.replace('%DATE%',r.result.data[i].date.toDate().toString('dd/MM/YYYY hh:mm'));
+							results=results.replace('%DATE%',r.result.data[i].date.toDate().toString('dd/MM/yyyy hh:mm'));
 							results=results.replace('%POSTER%',r.result.data[i].nomprenom);
 							results=results.replace('%COMMENT%',r.result.data[i].blog);
 							tpl.push(results);
