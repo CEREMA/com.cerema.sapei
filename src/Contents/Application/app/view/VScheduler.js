5b86c8ff4c2c2625517e1f13eb2fd5e8bb45a734
Stéphane Zucatti <stephane.zucati@me.com>
2016-02-02 10:13:39 +0100

    Update# 327

--- src/Contents/Application/app/view/VScheduler.js
+++ src/Contents/Application/app/view/VScheduler.js
@@ -181,7 +181,7 @@ App.view.define('VScheduler', {
 					sortable: true,
 					field: new Ext.form.TextField()
 				}],
-				eventStore: App.eventstore.create("sapei://schedule_users{Id, ResourceId,StartDate,EndDate,User,IF(User='"+Auth.User.uid+"';'blue';'red')=Cls}",{autoLoad: true}),
+				eventStore: App.eventstore.create("sapei://schedule_users{Id, ResourceId,StartDate,EndDate,User,IF(User='"+Auth.User.uid+"';'blue';'red')=Cls}"),
 				resourceStore: App.resourcestore.create("sapei://user{Id_Agent=Id,Id_Agent->bpclight_agents{nom+' '+prenom=NomPrenom+}}?Id_role_agent=2",{autoLoad: true})
 			}
 			]
