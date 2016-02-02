ecccd1c623541757d0fb9373658c41398915a30f
Stéphane Zucatti <stephane.zucati@me.com>
2016-02-02 16:43:55 +0100

    Update# 370

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -854,11 +854,10 @@ App.controller.define('CMain', {
 				if (r.data.length!=App.get('TAffaire').wiki) {
 					App.DB.get('sapei://wiki{date-,blog,poster->bpclight_agents{prenom+" "+nom=nomprenom}}?job='+AFFAIRE_ID,function(e,r) {
 						var html='<li><p class="timeline-date">%DATE%</p><div class="timeline-content"><h3>%POSTER%</h3><p>%COMMENT%</p></div></li>';
-                        console.log(r.result.data[i].date.split('T'));
 						var tpl=[];
 						for (var i=0;i<r.result.data.length;i++) {
 							var results=html;
-							results=results.replace('%DATE%',r.result.data[i].date.split('T')[0]+' '+r.result.data[i].date.split('T')[1]);
+							results=results.replace('%DATE%',r.result.data[i].date.toDate().toString('dd/mmm/YYYY hh:mm:ss'));
 							results=results.replace('%POSTER%',r.result.data[i].nomprenom);
 							results=results.replace('%COMMENT%',r.result.data[i].blog);
 							tpl.push(results);
