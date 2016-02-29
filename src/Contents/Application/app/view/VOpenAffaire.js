d1755e545645d9d04e968b27f1ca83abc1656df6
Stéphane Zucatti <stephane.zucatti@me.com>
2016-02-29 15:50:10 +0100

    Update# 432

--- src/Contents/Application/app/view/VOpenAffaire.js
+++ src/Contents/Application/app/view/VOpenAffaire.js
@@ -80,14 +80,14 @@ App.view.define('VOpenAffaire', {
                         value = 0;
                     };              
                     var id=Ext.id();
-                    /*var pt = (value*100/record.data.total)/100;*/
+                    var pt = 10;
                     var id = Ext.id();
                     Ext.defer(function (id,pt) {
                         var p = Ext.create('Ext.ProgressBar',{
                             renderTo: id,
                             animate: true,
                             width: '100%',
-                            value: 0.5,
+                            value: pt,
                             text: (pt*100)+"%",
                         });                        
                     }, 50, undefined, [id,pt]);
