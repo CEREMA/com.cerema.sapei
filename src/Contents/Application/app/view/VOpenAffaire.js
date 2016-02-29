ce858f2130415e56982065ffc0b555696a0f5d3a
Stéphane Zucatti <stephane.zucatti@me.com>
2016-02-29 16:29:14 +0100

    Update# 438

--- src/Contents/Application/app/view/VOpenAffaire.js
+++ src/Contents/Application/app/view/VOpenAffaire.js
@@ -109,7 +109,7 @@ App.view.define('VOpenAffaire', {
                    groupHeaderTpl: '{name}'
                 })
             ],            
-			store: App.store.create('sapei://job{Id_job,avancement, total,axe.Axe,axe.dpt.Lib_dpt,Intitule_job+,Id_pilote_job->bpclight_agents{nom+" "+prenom=nomprenom},job_statut.statut,type_prestation.Libelle_type_prestation,Date_prise_job,Date_livrable_job,Num_SIGMA_job,client_origine.Lib_client_origine}',{groupField:"Lib_client_origine"})
+			store: App.store.create('sapei://job{Id_job,avancement, total,axe.Axe,axe.dpt.Lib_dpt,Intitule_job+,job_statut.statut,type_prestation.Libelle_type_prestation,Date_prise_job,Date_livrable_job,Num_SIGMA_job,client_origine.Lib_client_origine,Id_pilote_job->bpclight_agents{nom+" "+prenom=nomprenom}}',{groupField:"Lib_client_origine"})
 		}
 		];
 
