82ef130272b72c1ddafbb2febe28ff43958ae9bf
Stéphane Zucatti <stephane.zucatti@me.com>
2016-02-29 16:43:47 +0100

    Update# 442

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -61,23 +61,28 @@ App.view.define('VMain', {
                 xtype: "tabpanel",
                 
                 items: [
-            {
-				xtype: "VSchedulerMain",
-                title: "Tab 1",
-				hidden: false,
-                bbar: [
-                    '->',
-                    {
-                        xtype: "panel",
-                        border: false,
-                        height: 35,
-                        html: '<table><tr><td><div style="background-color:yellow;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Etudes</td><td><div style="background-color:orange;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Mesures</td><td><div style="background-color:red;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Traitement</td></tr></table>',
-                        bodyStyle: "background-color:transparent",
-                        itemId: "Legendes"
-                    }
-                ],                            
-				flex: 1                
-            }                    
+                {
+                    xtype: "VSchedulerMain",
+                    title: "Planification",
+                    hidden: false,
+                    bbar: [
+                        '->',
+                        {
+                            xtype: "panel",
+                            border: false,
+                            height: 35,
+                            html: '<table><tr><td><div style="background-color:yellow;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Etudes</td><td><div style="background-color:orange;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Mesures</td><td><div style="background-color:red;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Traitement</td></tr></table>',
+                            bodyStyle: "background-color:transparent",
+                            itemId: "Legendes"
+                        }
+                    ],                            
+                    flex: 1                
+                },
+                {
+                    xtype: "VOpenAffaire",
+                    border: false,
+                    title: "Affaires"
+                }
                 ]
             },
 			]
