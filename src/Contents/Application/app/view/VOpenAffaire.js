6d9d36f4ee569f2cdf9febd84946315050c22bb4
Stéphane Zucatti <stephane.zucatti@me.com>
2016-02-29 16:14:09 +0100

    Update# 436

--- src/Contents/Application/app/view/VOpenAffaire.js
+++ src/Contents/Application/app/view/VOpenAffaire.js
@@ -93,14 +93,19 @@ App.view.define('VOpenAffaire', {
                     }, 50, undefined, [id,pt]);
                     return "<div id='" + id + "'></div>";
 				}
-			}
+			},
+            {
+                text: "Statut",
+                dataIndex: "statut",
+                width: 100
+            }
 			],
             features: [
                 Ext.create('Ext.grid.feature.Grouping',{
                    groupHeaderTpl: '{name}'
                 })
             ],            
-			store: App.store.create('sapei://job{Id_job,avancement, total,axe.Axe,axe.dpt.Lib_dpt,Intitule_job+,type_prestation.Libelle_type_prestation,Date_prise_job,Date_livrable_job,Num_SIGMA_job,client_origine.Lib_client_origine}',{groupField:"Lib_client_origine"})
+			store: App.store.create('sapei://job{Id_job,avancement, total,axe.Axe,axe.dpt.Lib_dpt,Intitule_job+,job_statut.statut,type_prestation.Libelle_type_prestation,Date_prise_job,Date_livrable_job,Num_SIGMA_job,client_origine.Lib_client_origine}',{groupField:"Lib_client_origine"})
 		}
 		];
 
