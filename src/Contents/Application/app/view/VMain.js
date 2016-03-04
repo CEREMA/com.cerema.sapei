bfaef89b8b81aef83abb5830cf2d6f26f33cdef7
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-04 15:45:17 +0100

    Update# 529

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -59,7 +59,7 @@ App.view.define('VMain', {
 				name: "Pilote",
                 field: "Id_pilote_job",
 				type: "choice",
-				model: 'sapei://user{bpclight_agents->{kage,nom+" "+prenom=NomPrenom+}}',
+				model: 'sapei://user{Id_agent->bpclight_agents->{kage,nom+" "+prenom=NomPrenom+}}',
 				display: "NomPrenom",
 				value: "kage"
 			 }			 
