2ddac753ccb13ac01d22d8a57ee9c17178fafe48
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-04 13:57:57 +0100

    Update# 513

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -209,8 +209,9 @@ App.controller.define('CMain', {
 	},
 	filter_onclick: function()
 	{
-		App.get('mainform FilterBox#FilterPanel').store=App.store.create("App.Jobs.getAll");
-        //App.get('VOpenAffaire grid#open').getStore();
+        var store=App.store.create("App.Jobs.getAll");
+        App.get('VOpenAffaire grid#open').setStore(store);
+		App.get('mainform FilterBox#FilterPanel').store=App.get('VOpenAffaire grid#open').getStore();
 		if (App.get('mainform FilterBox#FilterPanel').isVisible())
 		App.get('mainform FilterBox#FilterPanel').hide();
 		else
