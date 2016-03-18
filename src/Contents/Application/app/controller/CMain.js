6fae3b5c5ee50a821242387776eac752754e948e
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-18 10:31:22 +0100

    Update# 564

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -284,6 +284,7 @@ App.controller.define('CMain', {
 		}          
     },
     load_affaire: function(p,rec) {
+        alert('x');
         if (rec.data.Type==1) App.DB.get('sapei://ope{Id_job}?Id_skills='+rec.data.Id,function(e,r) {
             var id=r.result.data[0].Id_job; 
             App.reset(App.get('TAffaire')); 
