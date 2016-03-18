71c2e74240b50aa933f96f5a9e842c46b577028a
Stéphane Zucatti <stephane.zucatti@me.com>
2016-03-18 14:02:07 +0100

    Update# 577

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -289,7 +289,6 @@ App.controller.define('CMain', {
             var id=r.result.data[0].Id_job; 
             App.reset(App.get('TAffaire')); 
             App.DB.get('sapei://job{*,axe.Axe,axe.dpt.IdDepartement}?Id_job='+id,App.get('TAffaire'),function(response) {
-                alert('x');
                 response=response.data[0];
                 App.get('TAffaire combo#cboservice').getStore().getProxy().extraParams.Id_client_origine=response.Id_contact_client;
                 App.get('TAffaire combo#cboservice').getStore().load();
@@ -766,7 +765,7 @@ App.controller.define('CMain', {
 		App.reset(App.get('TAffaire')); 
         App.get('mainform FilterBox#FilterPanel').hide();
 		App.DB.get('sapei://job{*,axe.Axe,axe.dpt.IdDepartement}?Id_job='+record.data.Id_job,App.get('TAffaire'),function(response) {
-            
+            console.log(response);
 			response=response.data[0];
             App.get('TAffaire combo#cboservice').getStore().getProxy().extraParams.Id_client_origine=response.Id_contact_client;
             App.get('TAffaire combo#cboservice').getStore().load();
