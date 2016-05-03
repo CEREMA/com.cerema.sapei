dd798c59f55f682be33109c1bf98a26dd8c21e7f
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-03 16:35:55 +0200

    Update# 690

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -853,7 +853,8 @@ App.controller.define('CMain', {
 		  App.get(me,'schedulergrid#schedule_materiels').setStart(d);
           App.get(me,'schedulergrid#schedule_agents').setStart(d);
           var e=new Date();
-          e.setDate(0);
+          if (new Date(App.get(me,'combo#selectAnnee').getValue(),1,1).getMonth() == new Date(annee,1,29).getMonth() ? 29 : 28) e.setDate(28); else e.setDate(0);
+          //e.setDate(0);
           e.setMonth(p.getValue());
           e.setYear(App.get(me,'combo#selectAnnee').getValue());
 		  App.get(me,'schedulergrid#schedule_materiels').setEnd(e);
@@ -867,7 +868,8 @@ App.controller.define('CMain', {
 		  App.get(me,'schedulergrid#schedule_materiels').setStart(d);
           App.get(me,'schedulergrid#schedule_agents').setStart(d);            
           var e=new Date();
-          e.setDate(0);
+          if (new Date(App.get(me,'combo#selectAnnee').getValue(),1,1).getMonth() == new Date(annee,1,29).getMonth() ? 29 : 28) e.setDate(28); else e.setDate(0);
+            //e.setDate(0);
           e.setMonth(App.get(me,'combo#selectMonth').getValue());
           e.setYear(p.getValue());
 		  App.get(me,'schedulergrid#schedule_materiels').setEnd(e);
