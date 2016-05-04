068821026b2cb8e9642d008f2d50ba51a892e9a8
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-04 15:56:08 +0200

    Update# 741

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -892,7 +892,7 @@ App.controller.define('CMain', {
           //if (new Date(App.get(me,'combo#selectAnnee').getValue(),1,1).getMonth() == new Date(App.get(me,'combo#selectAnnee').getValue(),1,29).getMonth() ? 29 : 28) e.setDate(0); else e.setDate(28);          
           e.setMonth(App.get(me,'combo#selectMonth').getValue());
           e.setYear(p.getValue());
-          e.setDate(LastDayOfMonth(p.getValue(),App.get(me,'combo#selectMonth').getValue())-1);
+          e.setDate(LastDayOfMonth(p.getValue(),App.get(me,'combo#selectMonth').getValue()));
           console.log(e);
 		  App.get(me,'schedulergrid#schedule_materiels').setEnd(e);
           App.get(me,'schedulergrid#schedule_agents').setEnd(e);
