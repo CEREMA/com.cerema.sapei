179abe46ddc49498e62308d680c847f9246103b3
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-04 15:44:18 +0200

    Update# 737

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -878,7 +878,6 @@ App.controller.define('CMain', {
                   else
                     return 28;
               };
-              alert(new Date(year, month, 0).getDate());
             return new Date(year, month, 0).getDate();
           };            
           var d=new Date();
@@ -891,7 +890,7 @@ App.controller.define('CMain', {
           if (new Date(App.get(me,'combo#selectAnnee').getValue(),1,1).getMonth() == new Date(App.get(me,'combo#selectAnnee').getValue(),1,29).getMonth() ? 29 : 28) e.setDate(0); else e.setDate(28);          
           e.setMonth(App.get(me,'combo#selectMonth').getValue());
           e.setYear(p.getValue());
-          e.setDate(LastDayOfMonth(p.getValue(),App.get(me,'combo#selectMonth').getValue()));
+          e.setDate(LastDayOfMonth(p.getValue()-1,App.get(me,'combo#selectMonth').getValue()));
             console.log(e);
 		  App.get(me,'schedulergrid#schedule_materiels').setEnd(e);
           App.get(me,'schedulergrid#schedule_agents').setEnd(e);
