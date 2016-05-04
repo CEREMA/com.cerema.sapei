2d7dbc0e4bb44d60b2fd97df1c77950620450413
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-04 15:21:15 +0200

    Update# 727

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -876,10 +876,10 @@ App.controller.define('CMain', {
           if (new Date(App.get(me,'combo#selectAnnee').getValue(),1,1).getMonth() == new Date(App.get(me,'combo#selectAnnee').getValue(),1,29).getMonth() ? 29 : 28) e.setDate(0); else e.setDate(28);          
           e.setMonth(App.get(me,'combo#selectMonth').getValue()-1);
           e.setYear(p.getValue());
-          e.setDate(LastDayOfMonth(p.getValue(),App.get(me,'combo#selectMonth').getValue()));
+          e.setDate(LastDayOfMonth(p.getValue(),App.get(me,'combo#selectMonth').getValue())-1);
             console.log(e);
-		  App.get(me,'schedulergrid#schedule_materiels').setEndDate(e);
-          App.get(me,'schedulergrid#schedule_agents').setEndDate(e);
+		  App.get(me,'schedulergrid#schedule_materiels').setEnd(e);
+          App.get(me,'schedulergrid#schedule_agents').setEnd(e);
         });
         // set scheduler begin date by minimum of date 
         App.DB.get('sapei://schedule_users{Id, Job, StartDate}?Job='+App.get('TAffaire').ItemID,function(e,r) { 
