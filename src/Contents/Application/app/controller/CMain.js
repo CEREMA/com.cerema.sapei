c02a599bbee04cf96238cc8d2ae7c7b5f6267734
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-04 15:53:32 +0200

    Update# 740

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -879,6 +879,7 @@ App.controller.define('CMain', {
                   else
                     return 28;
               };
+              alert(new Date(year, month, 0).getDate());
             return new Date(year, month, 0).getDate();
           };            
           var d=new Date();
