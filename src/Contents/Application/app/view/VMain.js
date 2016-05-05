098cde650951cac0373f29e6fb692dae77c533a4
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-05 19:59:52 +0200

    Update# 761

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -1,133 +1,159 @@
 App.view.define('VMain', {
 
-    extend: 'Ext.Panel',
-	alias : 'widget.mainform',
-	border: false,
-	
+	extend: 'Ext.Panel'
+	, alias: 'widget.mainform'
+	, border: false,
+
 	layout: "border",
-	
+
 	items: [
 		{
-			region: 'north',
-			height: 25,
-			minHeight: 25,
-			border:false,
-			baseCls: 'cls-header',
-			xtype: "Menu",
-			itemId: "MenuPanel",
-			menu: [
+			region: 'north'
+			, height: 25
+			, minHeight: 25
+			, border: false
+			, baseCls: 'cls-header'
+			, xtype: "Menu"
+			, itemId: "MenuPanel"
+			, menu: [
 				{
-					text: "Affaires",
-					menu: [
+					text: "Affaires"
+					, menu: [
 						{
-							itemId: "mnu_aff_new",
-							text: "Nouveau",
-                            hidden: true
-						},
-						{
-							itemId: "mnu_aff_close",
-							text: "Fermer",
-                            hidden: true
-						}                        
+							itemId: "mnu_aff_new"
+							, text: "Nouveau"
+							, hidden: true
+						}
+						, {
+							itemId: "mnu_aff_close"
+							, text: "Fermer"
+							, hidden: true
+						}
 					]
-				},
-                {
-                    text: "Contacts",
-                    menu: [
-                        {
-                            itemId: "mnu_contacts",
-                            text: "Carnet d'adresses"
+				}
+				, {
+					text: "Contacts"
+					, menu: [
+						{
+							itemId: "mnu_contacts"
+							, text: "Carnet d'adresses"
                         }
                     ]
                 }
 			]
-		},
-        {
-             region: "north"  ,
-             xtype: "FilterBox",
-			 itemId: "FilterPanel",
-			 fields: [
-			 {
-				name: "Axe",
-                field: "Axe",
-				type: "choice",
-				model: "sapei://axe",
-				display: "Axe",
-				value: "Axe"
-			 },
-			 {
-				name: "Pilote",
-                field: "Id_pilote_job",
-				type: "choice",
-				model: 'sapei://user{Id_agent=kage,Id_agent->bpclight_agents{nom+" "+prenom=NomPrenom+}}',
-				display: "NomPrenom",
-				value: "kage"
-			 }/*,
-			 {
-				name: "Agents",
-                field: "Id_users",
-				type: "choice",
-				model: 'sapei://user{Id_user,Id_agent->bpclight_agents{nom+" "+prenom=NomPrenom+}}',
-				display: "NomPrenom",
-				value: "Id_user"
-			 }*/,
-			 {
-				name: "Clients",
-                field: "Id_contact_client",
-				type: "choice",
-				model: 'sapei://client_origine{Id_client_origine,Lib_client_origine}',
-				display: "Lib_client_origine",
-				value: "Id_client_origine"
-			 }/*,
-			 {
-				name: "N° Affaire",
-                field: "Id_job",
-				type: "text"
-			 }  */               
-        ]            
-        },
-		{
-			region: "center",			
-			split:true,
-			layout: "fit",
-            border: false,
-			items: [
-			{
-				xtype: "TAffaire",
-				hidden: true,
-				flex: 1
-			},
-            {
-                xtype: "tabpanel",
-                itemId: "mainscreen",
-                activeTab: 0,
-                items: [
-                {
-                    xtype: "VOpenAffaire",
-                    border: false,
-                    title: "Affaires"
-                },                    
-                {
-                    xtype: "VSchedulerMain",
-                    title: "Planification",
-                    hidden: false,
-                    bbar: [
-                        '->',
-                        {
-                            xtype: "panel",
-                            border: false,
-                            height: 35,
-                            html: '<table><tr><td><div style="background-color:yellow;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Etudes</td><td><div style="background-color:orange;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Mesures</td><td><div style="background-color:red;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Traitement</td></tr></table>',
-                            bodyStyle: "background-color:transparent",
-                            itemId: "Legendes"
+		}
+		, {
+			region: "north"
+			, xtype: "FilterBox"
+			, itemId: "FilterPanel"
+			, fields: [
+				{
+					name: "Axe"
+					, field: "Axe"
+					, type: "choice"
+					, model: "sapei://axe"
+					, display: "Axe"
+					, value: "Axe"
+			 }
+				, {
+					name: "Pilote"
+					, field: "Id_pilote_job"
+					, type: "choice"
+					, model: 'sapei://user{Id_agent=kage,Id_agent->bpclight_agents{nom+" "+prenom=NomPrenom+}}'
+					, display: "NomPrenom"
+					, value: "kage"
+			 }
+				/*,
+							 {
+								name: "Agents",
+				                field: "Id_users",
+								type: "choice",
+								model: 'sapei://user{Id_user,Id_agent->bpclight_agents{nom+" "+prenom=NomPrenom+}}',
+								display: "NomPrenom",
+								value: "Id_user"
+							 }*/
+				
+				, {
+					name: "Clients"
+					, field: "Id_contact_client"
+					, type: "choice"
+					, model: 'sapei://client_origine{Id_client_origine,Lib_client_origine}'
+					, display: "Lib_client_origine"
+					, value: "Id_client_origine"
+			 }
+				/*,
+							 {
+								name: "N° Affaire",
+				                field: "Id_job",
+								type: "text"
+							 }  */
+        ]
+        }
+		, {
+			region: "center"
+			, split: true
+			, layout: "fit"
+			, border: false
+			, items: [
+				{
+					xtype: "TAffaire"
+					, hidden: true
+					, flex: 1
+			}
+				, {
+					xtype: "tabpanel"
+					, itemId: "mainscreen"
+					, activeTab: 0
+					, items: [
+						{
+							xtype: "VOpenAffaire"
+							, border: false
+							, title: "Affaires"
+                		}
+						, 
+						{
+							xtype: "VSchedulerMain"
+							, title: "Planification"
+							, hidden: false
+							, tbar: [
+								{
+									xtype: "combo"
+									, fieldLabel: "Année"
+									, labelAlign: "top"
+									, itemId: "selectAnnee"
+									, width: 70
+									, editable: false
+									, displayField: "year"
+									, valueField: "year"
+									, margin: {
+										bottom: 5
+										, right: 5
+									}
+									, store: App.store.create({
+										fields: [
+                        				"year"
+                    					]
+										, data: []
+									})
+            					}
+							]
+							, bbar: [
+                        '->'
+								, {
+									xtype: "panel"
+									, border: false
+									, height: 35
+									, html: '<table><tr><td><div style="background-color:yellow;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Etudes</td><td><div style="background-color:orange;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Mesures</td><td><div style="background-color:red;height:15px;width:15px;margin:5px">&nbsp;&nbsp;&nbsp;&nbsp;</div></td><td>Traitement</td></tr></table>'
+									, bodyStyle: "background-color:transparent"
+									, itemId: "Legendes"
                         }
-                    ],                            
-                    flex: 1                
+                    ]
+							, flex: 1
                 }
                 ]
-            },
-			]
+            }
+			, ]
 		}
 	]
-	
+
 });
