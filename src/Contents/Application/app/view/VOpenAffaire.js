cf1abe6ad94be2f45436d39c77595043cba7e5da
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-10 10:15:46 +0200

    Update# 854

--- src/Contents/Application/app/view/VOpenAffaire.js
+++ src/Contents/Application/app/view/VOpenAffaire.js
@@ -109,7 +109,7 @@ App.view.define('VOpenAffaire', {
 			],
             features: [
                 Ext.create('Ext.grid.feature.Grouping',{
-                   groupHeaderTpl: '{name} {values.rs.length}'
+                   groupHeaderTpl: '{name} {rows.length} ligne{[values.rows.length > 1 ? "s" : ""]})'
                 })
             ],            
 			store: App.store.create('sapei://job{Id_job,avancement, total,axe.Axe,axe.dpt.Lib_dpt,Intitule_job+,job_statut.statut,type_prestation.Libelle_type_prestation,Date_prise_job,Date_livrable_job,Num_SIGMA_job,client_origine.Lib_client_origine,Id_pilote_job->bpclight_agents{nom+" "+prenom=nomprenom}}',{groupField:"Lib_client_origine"})
