24614e1153eb359a109872901a75f7db3ba28e42
Stéphane Zucatti <stephane.zucatti@me.com>
2016-05-10 13:55:15 +0200

    Update# 872

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -84,7 +84,7 @@ App.view.define('VMain', {
                     name: "Matériels",
                     field: "Id_skills",
                     type: "choice",
-                    model: 'sapei://porteur_outils{Id_porteur_outils,porteur.libelle+"/"+outils.Libelle_materiel=Materiels+}',
+                    model: 'sapei://porteur_outils{Id_porteur_outils,porteur.porteur_libelle+"/"+outils.Libelle_materiel=Materiels+}',
                     display: "Matériels",
                     value: "Id_porteur"
                  }
